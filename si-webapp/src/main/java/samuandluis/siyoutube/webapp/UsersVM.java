d3a9731333def3a8286dd8f2fc6de9463d997d0a
srconde <srconde@esei.uvigo.es>
2017-12-09 20:40:56 +0100

    Added functions equivalent to DepartmentsVM ones

--- si-webapp/src/main/java/samuandluis/siyoutube/webapp/UsersVM.java
+++ si-webapp/src/main/java/samuandluis/siyoutube/webapp/UsersVM.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 
+import org.zkoss.bind.annotation.BindingParam;
+import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
 
 import samuandluis.siyoutube.webapp.DesktopEntityManagerManager;
 import samuandluis.siyoutube.persistence.User;
@@ -15,6 +18,11 @@ public class UsersVM {
 	private EntityManager em;
 	private Users users;
 	
+	private boolean isEditing = false;
+	
+	// User under edition...
+	private User currentUser;
+	
 	@Init
 	public void init() {
 		this.em = DesktopEntityManagerManager.getDesktopEntityManager();
@@ -23,5 +31,54 @@ public class UsersVM {
 	
 	public List<User> getUsers() {
 		return this.users.findAll();
+	}
+	
+	public User getCurrentUser() {
+		return currentUser;
+	}
+	
+	public void setCurrentUser(User currentUser) {
+		this.currentUser = currentUser;
+	}
+	
+	@Command
+	@NotifyChange("currentUser")
+	public void newEmployee() {
+		this.isEditing = false;
+		this.currentUser = new User();
+	}
+	
+	@Command
+	@NotifyChange("currentUser")
+	public void resetEditing() {
+		this.currentUser = null;
+	}
+	
+	@Command
+	@NotifyChange({"currentUser", "users"})
+	public void saveUser() {
+		
+		this.em.getTransaction().begin();
+			if (!isEditing) {
+				this.users.addNewUser(this.currentUser);
+			}
+		this.em.getTransaction().commit();
+		
+		this.currentUser = null;
+	}
+	
+	@Command
+	@NotifyChange("users")
+	public void delete(@BindingParam("u") User User) {
+		this.em.getTransaction().begin();
+			this.users.deleteUser(User);
+		this.em.getTransaction().commit();
+	}
+	
+	@Command
+	@NotifyChange("currentUser")
+	public void edit(@BindingParam("u") User User) {
+		this.isEditing = true;
+		this.currentUser = User;
 	}
 }
